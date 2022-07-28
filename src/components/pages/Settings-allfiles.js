3658ab55d18bac6ee00c96a9c6349d035802940f
Craig-Glaze <c.hall95@hotmail.co.uk>
2022-07-28 12:39:39 +0100

    Update Settings-allfiles.js

--- src/components/pages/Settings-allfiles.js
+++ src/components/pages/Settings-allfiles.js
@@ -1,6 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../App.css';
+import SidebarMenu from '../SidebarMenu';
+import ViewerOuterNFT from '../ViewerOuterNFT';
+import LoginBox from '../LoginBox';
+import NavbarFiles from '../navbarFiles';
 
-export default function Products() {
-    return <h1 className='products'>PRODUCTS</h1>;
+
+export default function NFTFolder() {
+
+
+    const [isDesktop, setDesktop] = useState(window.innerWidth > 1450);
+
+    const updateMedia = () => {
+        setDesktop(window.innerWidth > 978);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+
+    }, []);
+
+
+    return (
+
+
+        <div>
+            {isDesktop ? (
+                <>
+
+                    <SidebarMenu />
+                    <ViewerOuterNFT />
+                </>
+            ) : (
+                <>
+                    <div className="mobile-wrap">
+                        <NavbarFiles />
+
+                        <ViewerOuterNFT />
+                    </div>
+
+                </>
+            )}
+        </div>
+
+
+
+
+
+
+    );
 }

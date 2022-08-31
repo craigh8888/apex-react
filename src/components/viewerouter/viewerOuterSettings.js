609f38690ae0efc80995d43c35ac506347b58074
craigh8888 <c.hall95@hotmail.co.uk>
2022-08-31 10:54:38 +0100

    Update viewerOuterSettings.js
    Added further fields to this.    - Outer settings wasn't being loaded for the settings page, switched this around now.

--- src/components/viewerouter/viewerOuterSettings.js
+++ src/components/viewerouter/viewerOuterSettings.js
@@ -3,14 +3,22 @@ import '../../styles/FolderViewerOuter.css';
 
 import ViewerInnerSettings from '../viewerinner/viewerInnerSettings.js';
 
-function FolderViewerOuterSettings({bundlrBalance}) {
+function FolderViewerOuterSettings({ viewName, viewDesc, bundlrAddress, bundlrBalance, fundBundlr, setOnramper }) {
     return (
 
         <div className='folder-viewer-outer'>
             <div className='overlay'>
                 <div className='viewer-inner'>
                     <div>
-                        <ViewerInnerSettings bundlrBalance={bundlrBalance}/>
+                        {/* <ViewerInnerSettings bundlrBalance={bundlrBalance}/> */}
+                        <ViewerInnerSettings
+                            viewName={"Settings"}
+                            viewDesc={"You can edit your Billing, Account Information and Notifications here."}
+                            bundlrAddress={bundlrAddress}
+                            bundlrBalance={bundlrBalance}
+                            fundBundlr={fundBundlr}
+                            setOnramper={setOnramper}
+                        />
                     </div>
                 </div>
             </div>

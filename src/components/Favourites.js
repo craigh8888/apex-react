450d1e08e1726ab9987367b94c78a4499bceaa37
craigh8888 <c.hall95@hotmail.co.uk>
2022-09-04 20:33:11 +0100

    Update Favourites.js

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -7,8 +7,13 @@ import NavbarFiles from './navbarFiles';
 import TempNav from './menus/tempNav';
 import ViewerInnerFavourites from './viewerinner/viewerInnerFavourites';
 import ViewerInner from './viewerinner/Viewer-Inner';
+import Table from './tables/Table';
+import PageHeader from './tables/pageHeader';
 
-export default function Favourites() {
+
+
+
+export default function Favourites(setInnerContent) {
 
     const [isDesktop, setDesktop] = useState(window.innerWidth > 1000);
 
@@ -29,14 +34,31 @@ export default function Favourites() {
             {isDesktop ? (
                 <>
 
-        <ViewerInner 
+                    <PageHeader
+                        viewName="Favourites"
+                        button1Class="button-top add-folder"
+                        button2Class="button-top upload-files"
+                        button1Func=""
+                        button2Func=""
+                        button1Text="My Button"
+                        button2Text="My Other Button"
+                        setInnerContent={setInnerContent}
+
+                    />
+                    <Table headerNames={["Icon", "Name", "Folder", "Created", "Size", "View File", "Actions"]}
+                    />
+
+
+
+
+                    {/* <ViewerInner 
         viewName={"Favorites"}
         buttons={[<button>My button</button>, <button>My other button</button>]}
         headerNames={["Icon","Name","Folder","Created,Size","File Count", "View File", "Actions"]}
         rows={demoLinks}
         pagination={demoPages}
         
-        />
+        /> */}
                 </>
             ) : (
                 <>

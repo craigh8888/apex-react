a418e97b849f6ee5974e096f8ad8dc5b3967dd4a
craigh8888 <c.hall95@hotmail.co.uk>
2022-09-05 14:34:16 +0100

    Update AccountSidebar.js

--- src/components/menus/AccountSidebar.js
+++ src/components/menus/AccountSidebar.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/account-sidebar.css';
 import AccountImage from '../../images/account-owl.png';
 import { ReactComponent as DownArrow } from '../../images/down-arrow.svg';
+import { ReactComponent as UpArrow } from '../../images/up-arrow.svg';
 import { Link } from 'react-router-dom';
 
+
 function AccountSidebar() {
+    const [isActive, setIsActive] = useState(false);
 
 
     return (
 
         <>
             <div className='account-wrap'>
-                <Link to='#'>
+
+                <Link to='#' onClick={() => setIsActive(!isActive)}>
+
                     <div>
 
                         <span>John S</span>
                         <img src={AccountImage} />
-                        <DownArrow />
+                        {isActive ? <UpArrow /> : <DownArrow />}
 
                     </div>
+                    {isActive && <div className='account-accord-content'>content here</div>}
                 </Link>
             </div>
         </>
